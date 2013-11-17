f42b309239d614e12c57d1578cfa9f8502fc6e3f
Isaya <isaya_ie@hotmail.com>
2013-11-18 00:29:42 +0100

    Various French translation typo fixes Addition of Linux/Mac conversion script Ability to pass the initial encoding of the tra files to the conversion script (provision for eastern europe translations)

--- bg1npc/conv_tra.bat
+++ bg1npc/conv_tra.bat
@@ -1,4 +1,5 @@
 :: Use a parameter to the script (%1) to specify the directory that must be converted
+:: Use a parameter to the script (%2) to specify the encoding of the files to convert
 
 :: Prevent display of DOS command lines
 @echo off
@@ -6,4 +7,4 @@
 :: Since the pattern matching in Windows considers that "fu.tra_utf8" also matches "*.tra", we have to
 :: handle the file extension by ourselfves in order to avoid all those tra_utf8_utf8 etc files
 :: Use %%~ni instead of %%~nxi (x is for extension) and specify the tra extension
-for %%i in (bg1npc\tra\%1\*.tra) do bg1npc\iconv -f CP1252 -t UTF-8 "bg1npc\tra\%1\%%~ni.tra" > "bg1npc\tra\%1\%%~ni.tra_utf8"
+for %%i in (bg1npc\tra\%1\*.tra) do bg1npc\iconv -f %2 -t UTF-8 "bg1npc\tra\%1\%%~ni.tra" > "bg1npc\tra\%1\%%~ni.tra_utf8"
