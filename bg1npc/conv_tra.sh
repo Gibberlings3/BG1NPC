e6a6e8276e2dead225e0ea0a7508a824a2ed4fba
Isaya <isaya_ie@hotmail.com>
2014-05-20 22:49:06 +0200

    Changed method to use an utf8 subdirectory for converted files

--- bg1npc/conv_tra.sh
+++ bg1npc/conv_tra.sh
@@ -1,7 +1,11 @@
 # Use a parameter to the script ($1) to specify the directory that must be converted
 # Use a parameter to the script ($2) to specify the encoding of the files to convert
+if [ ! -d bg1npc/tra/$1/utf8 ]; then
+	mkdir bg1npc/tra/$1/utf8
+fi
+
 for file in $(ls bg1npc/tra/$1/*.tra) 
 do
 #  echo ${file/tra\/$1\//}
-  iconv -f "$2" -t UTF-8  "$file" > "$file"_utf8
+  iconv -f "$2" -t UTF-8 "$file" > utf8/"$file"
 done
