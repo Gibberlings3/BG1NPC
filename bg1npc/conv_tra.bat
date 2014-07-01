2c798bbde6ca8504ac28730ccae857fd4198be5d
Bryan Dunne <astrobryguy@gmail.com>
2014-06-30 21:28:21 -0500

    tra file converter update
    conv_tra.bat and conv_tra.sh use icon to convert .tra files to UTF-8.
    Adding “-c” option to make iconv discard unconvertible characters
    silently.  This avoids a situation where icon stops due to an error,
    leaving an incomplete .tra file.
    
    http://forums.gibberlings3.net/index.php?showtopic=26388&p=224307

--- bg1npc/conv_tra.bat
+++ bg1npc/conv_tra.bat
@@ -9,4 +9,4 @@ if not exist bg1npc\tra\%1\utf8 mkdir bg1npc\tra\%1\utf8
 :: Since the pattern matching in Windows considers that "fu.tra_utf8" also matches "*.tra", we have to
 :: handle the file extension by ourselfves in order to avoid all those tra_utf8_utf8 etc files
 :: Use %%~ni instead of %%~nxi (x is for extension) and specify the tra extension
-for %%i in (bg1npc\tra\%1\*.tra) do bg1npc\iconv -f %2 -t UTF-8 "bg1npc\tra\%1\%%~ni.tra" > "bg1npc\tra\%1\utf8\%%~ni.tra"
+for %%i in (bg1npc\tra\%1\*.tra) do bg1npc\iconv -c -f %2 -t UTF-8 "bg1npc\tra\%1\%%~ni.tra" > "bg1npc\tra\%1\utf8\%%~ni.tra"
