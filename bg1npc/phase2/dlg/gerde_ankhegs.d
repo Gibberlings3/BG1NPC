0944becf57bee3be54578930ce98116ebe807b9b
Gitjas <Gitjas@users.noreply.github.com>
2021-05-16 18:07:26 +0200

    Kivan fixes
    Kivan should count Gerde's ankheg kills correctly.
    Kivan should not reference Prism in Mutamin's garden if he didn't meet Prism.

---
+++ bg1npc/phase2/dlg/gerde_ankhegs.d
@@ -0,0 +1,2 @@
+/* add tracking variable to Gerde because the original's game variable is too unreliable */
+ADD_TRANS_ACTION GERDE BEGIN 0 END BEGIN END ~SetGlobal("X#GerdeAnkhegsCount","GLOBAL",1)~
